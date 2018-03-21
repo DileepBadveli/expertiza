31f1d5a7fa05e446f67e771de569374a8640cd57
mpan2 <mpan2@ncsu.edu>
2018-03-21 17:26:15 -0400

    add expect to compute_total_score

--- spec/models/on_the_fly_cakc_spec.rb
+++ spec/models/on_the_fly_cakc_spec.rb
@@ -4,6 +4,7 @@ describe '#compute_total_score' do
 it 'return total scores' do
   @score={"1": {scores: {avg: 4.5}}}
   allow(Questionnaire).to receive(:get_weighted_score).with(assignment,@score).and_return(4.5)
+  expect(Questionnaire.get_weighted_score(assignment,@score)).to eq(4.5)
 end
 end
 describe '#compute_reviews_hash' do
