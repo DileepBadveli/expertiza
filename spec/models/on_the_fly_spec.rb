37a69b83204e9539ef5307086bdc2e2038851913
mpan2 <mpan2@ncsu.edu>
2018-03-25 15:29:26 -0400

    add features to compute review hashes

--- spec/models/on_the_fly_spec.rb
+++ spec/models/on_the_fly_spec.rb
@@ -1,30 +1,25 @@
 describe OnTheFlyCalc do
- let(:assignment) { build(:assignment, id: 1,)}
- let(:Assignment){ Assignment.new}
- Assignment.extend(OnTheFlyCalc)
+ let(:assignment) { build(:assignment, id: 1, assignment_questionaire: assignment_questionnaire, questionaire: questionaire)}
+ let(:assignment_questionnaire) { build(:assignment_questionnaire, used_in_round: 2)}
+ let(:assignment2) { build(:assignment, id: 2, assignment_questionaire: assignment_questionnaire2)}
+ let(:assignment_questionnaire2) { build(:assignment_questionnaire, used_in_round: 1)
+ let(:questionnaire) { build(:questionnaire)}
  describe '#compute_total_score' do
  it 'return total scores' do
    @score={"1"=>{:scores=>{:avg=>4.5}}}
    allow(Questionnaire).to receive(:get_weighted_score).with(assignment,@score).and_return(4.5)
-   expect(Questionnaire.get_weighted_score(assignment,@score)).to eq(4.5)
+   expect(questionnaire.get_weighted_score(assignment,@score)).to eq(4.5)
  end
  end
  describe '#compute_reviews_hash' do
-   context "Has multiple review phases with different review rubrics" do
-     it "calls scores_varying_rubrics" do
-       allow(Assignment).to receive(:varying_rubrics_by_round?).and_return(true)
-       Assignment.compute_reviews_hash
-       expect(Assignment).to receive(:scores_varying_rubrics)
-     end
-   end
-   context "Does not have multiple review phases with different review rubrics" do
-     it "calls scores_non_varying_rubrics" do
-       allow(Assignment).to receive(:varying_rubrics_by_round?).and_return(false)
-       Assignment.compute_reviews_hash
-       expect(Assignment).to receive(:scores_non_varying_rubrics)
-     end
-   end
-
+  it 'for varying_rubrics_by_round case' do
+    allow(Assignment).to receive(:varying_rubrics_by_round?).and_return(true)
+    expect(assignment.varying_rubrics_by_round?).to eq(true)
+  end
+  it 'for non-varying_rubrics_by_round case' do
+    allow(Assignment).to receive(:varying_rubrics_by_round?).and_return(false)
+    expect(assignment2.varying_rubrics_by_round?).to eq(false)
+  end
  end
  describe '#compute_avg_and_ranges_hash' do
 
